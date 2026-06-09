f6c41e853845864e46e2abca85abcbae89705270
Raju C <99310051+rajusilva@users.noreply.github.com>
2026-06-09 13:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def select_best_candidate_with_gemini(english_grievance, candidates):
     Do not include markdown formatting or backticks around the JSON.
     """
     response = ai_client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-3.1-flash-lite',
         contents=prompt,
         config=types.GenerateContentConfig(response_mime_type="application/json")
     )
